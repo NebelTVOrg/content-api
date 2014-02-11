88a2e4b5645687b4f3c55c8f56aa0bc2d398091d
Antonio Shlapak <antonio.shlapak@gmail.com>
2014-02-11 17:24:20 +0200

    NebelTVContentAPI TestSuite (initial draft): JSON validations

---
+++ NebelTVContentAPI/test/com/nebel_tv/content/NebelTVContentAPITestSuite.java
@@ -0,0 +1,35 @@
+package com.nebel_tv.content;
+
+import com.nebel_tv.content.api.MediaWrapperTest;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    MediaWrapperTest.class
+})
+public class NebelTVContentAPITestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
---
+++ NebelTVContentAPI/test/com/nebel_tv/content/api/MediaWrapperTest.java
@@ -0,0 +1,128 @@
+
+package com.nebel_tv.content.api;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class MediaWrapperTest {
+    
+    /**
+     * 
+     */
+    public MediaWrapperTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getMediaData (<code>getMedias</code>) method, of class MediaWrapper.
+     */
+    @Test
+    public void testGetMediaDataGetMedias() {
+        System.out.println("getMediaData");
+        final String url = "http://54.201.170.111:8080/IvaWrapperWeb/getMedias?skip=100&n=3&category=0";
+        
+        MediaWrapper instance = new MediaWrapper();
+        MediaWrapperResponse result = instance.getMediaData(url);
+        assertTrue(result.responseType == MediaWrapperResponse.ResponseType.NA);       
+        assertTrue(result.responseResult == MediaWrapperResponse.ResponseResult.Ok);
+        
+        try {
+            JSONArray jsonGetMedias = new JSONArray(result.responseData);
+            assertFalse(jsonGetMedias.length() == 0);
+            
+            for (int i = 0; i < jsonGetMedias.length(); i++) {
+                JSONObject jsonItem = jsonGetMedias.getJSONObject(i);
+                testJsonMediaItem(jsonItem);
+            }
+        } catch (JSONException e) {
+            fail("JSON parsing failed" + e.getMessage());
+        }
+    }
+    
+    /**
+     * Test of getMediaData (<code>getMediaItem</code>) method, of class MediaWrapper.
+     */
+    @Test
+    public void testGetMediaDataGetMediaItem() {
+        final String url = "http://54.201.170.111:8080/IvaWrapperWeb/getMediaItem?n=2";
+        
+        MediaWrapper instance = new MediaWrapper();
+        MediaWrapperResponse result = instance.getMediaData(url);
+        assertTrue(result.responseType == MediaWrapperResponse.ResponseType.NA);       
+        assertTrue(result.responseResult == MediaWrapperResponse.ResponseResult.Ok);
+        
+        try {
+            JSONObject jsonItem = new JSONObject(result.responseData);
+            testJsonMediaItem(jsonItem);
+
+        } catch (JSONException e) {
+            fail("JSON parsing failed" + e.getMessage());
+        }        
+    }  
+    
+    /**
+     * Test of getUrlLastSegment (<code>getMedias</code>) method, of class MediaWrapper.
+     */
+    @Test
+    public void testGetUrlLastSegmentGetMedias() {
+        String urlGetMedias = "http://54.201.170.111:8080/IvaWrapperWeb/getMedias?skip=100&n=3&category=0";
+        String result = MediaWrapper.getUrlLastSegment(urlGetMedias);
+        assertEquals(result, "getMedias");
+        
+        urlGetMedias = "http://google.com/IvaWrapperWeb/getMedias?skip=100&n=3&category=0";
+        result = MediaWrapper.getUrlLastSegment(urlGetMedias);
+        assertEquals(result, "getMedias");        
+    }
+    
+    /**
+     * Test of getUrlLastSegment (<code>getMediaItem</code>) method, of class MediaWrapper.
+     */
+    @Test
+    public void testGetUrlLastSegmentGetMediaItem() {        
+        String urlGetMediaItem = "http://54.201.170.111:8080/IvaWrapperWeb/getMediaItem?n=2";
+        String result = MediaWrapper.getUrlLastSegment(urlGetMediaItem);
+        assertEquals(result, "getMediaItem");
+        
+        urlGetMediaItem = "http://pravda.com.ua/IvaWrapperWeb/getMediaItem?n=2";
+        result = MediaWrapper.getUrlLastSegment(urlGetMediaItem);
+        assertEquals(result, "getMediaItem");                
+    }
+
+    /**
+     * Test of JSON presentation of the media items
+     * The following keys are mandatory <code>media_id, title, author, date</code>
+     */
+    private void testJsonMediaItem(JSONObject jsonItem) {
+        assertNotNull(jsonItem);
+        
+        assertTrue(jsonItem.has("media_id"));
+        assertTrue(jsonItem.has("title"));
+        assertTrue(jsonItem.has("author"));
+        assertTrue(jsonItem.has("date"));        
+    }
+}
