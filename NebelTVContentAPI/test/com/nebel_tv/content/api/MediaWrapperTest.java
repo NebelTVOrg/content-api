5b7caf7a2081fe32159f95ce522f3549c9fafa9b
Antonio Shlapak <antonio.shlapak@gmail.com>
2014-02-11 19:44:02 +0200

    Minor: headers with copyright

--- NebelTVContentAPI/test/com/nebel_tv/content/api/MediaWrapperTest.java
+++ NebelTVContentAPI/test/com/nebel_tv/content/api/MediaWrapperTest.java
@@ -1,4 +1,19 @@
-
+/**
+ * Copyright (C) 2014 Nebel TV (http://nebel.tv)
+    
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+    
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+    
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.nebel_tv.content.api;
 
 import org.json.JSONArray;
@@ -79,7 +94,6 @@ public class MediaWrapperTest {
         try {
             JSONObject jsonItem = new JSONObject(result.responseData);
             testJsonMediaItem(jsonItem);
-
         } catch (JSONException e) {
             fail("JSON parsing failed" + e.getMessage());
         }        
@@ -114,7 +128,7 @@ public class MediaWrapperTest {
     }
 
     /**
-     * Test of JSON presentation of the media items
+     * Test of JSON presentation of the media item
      * The following keys are mandatory <code>media_id, title, author, date</code>
      */
     private void testJsonMediaItem(JSONObject jsonItem) {
